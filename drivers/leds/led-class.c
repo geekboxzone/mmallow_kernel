b1b34c3ac6efab7ad1666efb008fa4d0548aeeb3
terry <terry@szwesion.com>
2016-06-15 10:13:50 +0800

    Leds: add the suppport for leds menu

--- drivers/leds/led-class.c
+++ drivers/leds/led-class.c
@@ -70,7 +70,7 @@ static struct device_attribute led_class_attrs[] = {
 	__ATTR(brightness, 0644, led_brightness_show, led_brightness_store),
 	__ATTR(max_brightness, 0444, led_max_brightness_show, NULL),
 #ifdef CONFIG_LEDS_TRIGGERS
-	__ATTR(trigger, 0644, led_trigger_show, led_trigger_store),
+	__ATTR(trigger, 0777, led_trigger_show, led_trigger_store),
 #endif
 	__ATTR_NULL,
 };
